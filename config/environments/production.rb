80205d7dfa965e15314fe28f2c92dfa7747e5bb6
Kath <you@example.com>
2015-08-17 15:09:54 +1000

    users no longer confirmable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,18 +63,18 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: "ancient-savannah-3859.herokuapp.com"}
-  # config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.smtp_settings = {
-    :user_name => '37276c17280200430',
-    :password => '88df6a8e865c07',
-    :address => 'mailtrap.io',
-    :domain => 'mailtrap.io',
-    :port => '2525',
-    :authentication => :cram_md5
-  }
+  # config.action_mailer.default_url_options = { host: "ancient-savannah-3859.herokuapp.com"}
+  # # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.smtp_settings = {
+  #   :user_name => '37276c17280200430',
+  #   :password => '88df6a8e865c07',
+  #   :address => 'mailtrap.io',
+  #   :domain => 'mailtrap.io',
+  #   :port => '2525',
+  #   :authentication => :cram_md5
+  # }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
